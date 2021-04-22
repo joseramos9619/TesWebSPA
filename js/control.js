3cfd791de58e836e2fc2d12f22a7bad74cd95d93
Jose Ramos <joseramos9619@gmail.com>
2021-04-22 01:21:32 -0500

    avances botones js

--- js/control.js
+++ js/control.js
@@ -1,14 +1,36 @@
 const botones = document.querySelectorAll("button");
 const h1 = document.querySelector("header > h1");
 const barra = document.querySelector("header > nav");
+const datos = document.querySelectorAll(".login input");
+const login = document.querySelector(".login");
+const test = document.querySelector("#test");
 
 botones.forEach((boton) => {
-    boton.addEventListener("click", (evento) => {
-        evento.preventDefault();
-        console.log(boton.id)
-        if (boton.id == "iniciar") {
-          h1.setAttribute("style","display: none;")
-        }
+  boton.addEventListener("click", (evento) => {
+    evento.preventDefault();
 
-    });
+    if (boton.id == "iniciar") {
+      if (datos[0].value != "" && datos[1].value != "") {
+        h1.setAttribute("style", "display: none;")
+        barra.querySelector("h2").insertAdjacentText("beforeend", ` ${datos[0].value} ${datos[1].value}`);
+        login.setAttribute("style", "display: none;");
+        barra.removeAttribute("style");
+        test.removeAttribute("style");
+      }
+      else {
+        alert("completar todos los campos de texto para ingresar");
+      }
+    }
+
+  });
 });
+
+barra.querySelector("#salir").addEventListener("click", (evento) => {
+  evento.preventDefault();
+  alert("al salir se pierde todo el prgreso del test");
+  login.removeAttribute("style");
+  h1.removeAttribute("style");
+  barra.setAttribute("style", "display: none;");
+  test.setAttribute("style", "display: none;");
+  barra.querySelector("h2").innerHTML=`<i class="far fa-user"></i>`;
+})
