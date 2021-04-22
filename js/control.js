ad56f18afb52e2fb5ad70d2d366e962c25992f21
Jose Ramos <joseramos9619@gmail.com>
2021-04-21 22:06:58 -0500

    avances en js

--- js/control.js
+++ js/control.js
@@ -1,14 +1,14 @@
-const barra = document.querySelector("#barra");
-const boton = document.querySelector("#btnver");
-const boton2 = document.querySelector("#btnocultar");
+const botones = document.querySelectorAll("button");
+const h1 = document.querySelector("header > h1");
+const barra = document.querySelector("header > nav");
 
-boton.addEventListener("click",(evento)=>{
-    evento.preventDefault();
+botones.forEach((boton) => {
+    boton.addEventListener("click", (evento) => {
+        evento.preventDefault();
+        console.log(boton.id)
+        if (boton.id == "iniciar") {
+          h1.setAttribute("style","display: none;")
+        }
 
-    barra.removeAttribute("style");
-})
-boton2.addEventListener("click",(evento)=>{
-    evento.preventDefault();
-
-    barra.setAttribute("style","display: none;")
-})
+    });
+});
